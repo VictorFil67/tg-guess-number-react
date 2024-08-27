44c6414860965beb3329ac66923193aebfd6fd5d
VictorFil67 <victorfilinovych@gmail.com>
2024-08-28 02:28:37 +0300

    added start game button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import axios from "axios";
 import "./App.css";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const tg = window.Telegram.WebApp;
 
 export const App = () => {
+  const [guess, setGuess] = useState("");
+  const [result, setResult] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+
   useEffect(() => {
     // Инициализация Telegram WebApp SDK
 
     tg.ready();
 
     // Ожидаем закрытие WebApp
+    tg.onEvent("web_app_close", () => {
+      console.log("WebApp closed");
+    });
+
+    return () => {
+      tg.offEvent("web_app_close");
+    };
   }, []);
+
+  const handleInputChange = (e) => {
+    setGuess(e.target.value);
+  };
 
   const handleClose = () => {
     tg.close();
@@ -20,7 +36,15 @@ export const App = () => {
   return (
     <div className="App">
       <h1>Вгадай число</h1>
-
+      {!gameStarted ? (
+        <button>Почати гру</button>
+      ) : (
+        <>
+          <input type="number" value={guess} onChange={handleInputChange} />
+          <button>Відправити</button>
+        </>
+      )}
+      {result && <p>{result}</p>}
       <button onClick={handleClose}>Закрити гру</button>
     </div>
   );
