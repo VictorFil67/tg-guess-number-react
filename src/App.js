b94c15c206f53339859acecfb9a8a48918403201
VictorFil67 <victorfilinovych@gmail.com>
2024-08-28 02:11:03 +0300

    added close game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useEffect } from "react";
+
+const tg = window.Telegram.WebApp;
+
+export const App = () => {
+  useEffect(() => {
+    // Инициализация Telegram WebApp SDK
+
+    tg.ready();
+
+    // Ожидаем закрытие WebApp
+  }, []);
+
+  const handleClose = () => {
+    tg.close();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Вгадай число</h1>
+
+      <button onClick={handleClose}>Закрити гру</button>
     </div>
   );
-}
+};
 
 export default App;
